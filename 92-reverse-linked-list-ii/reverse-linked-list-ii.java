4e73d0e847de0106a2ef8b3252ce66ca26c842ae
Sarthak <sarthak.kamtikar@gmail.com>
2026-09-20 12:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (29.37%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,103 +1,70 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         int count = 1;
         ListNode temp = head;
         ListNode temp2 = head;
-        Stack <ListNode> stack = new Stack<>();
+        Stack<ListNode> stack = new Stack<>();
 
-        if(left == right){
+        if (left == right) {
             return head;
         }
 
-
-
-
-
-
-       // for putting temp2 at r 
-       while(count < right){
+        // Move temp2 to right
+        while (count < right) {
             temp2 = temp2.next;
             count++;
-       }
-       // now temp2 is at r
+        }
 
-       count = 1;
-       //for putting in stack
-       while(count < left){
-        temp = temp.next;
-        count++;
-       }
+        count = 1;
 
-       // now count is at left 
-       while(count <= right ){
+        // Move temp to left
+        while (count < left) {
+            temp = temp.next;
+            count++;
+        }
+
+        // Push left to right into stack
+        while (count <= right) {
             stack.push(temp);
             temp = temp.next;
             count++;
-       }
+        }
 
-       // now temp is pointing to 5
-       temp2.next = temp;
-       temp2 = temp2.next;
+        // Save the node after right
+        temp2.next = temp;
+        temp2 = temp2.next;
 
-       
-       // fixed temp2.next here
+        // Reverse
+        temp = head;
+        count = 1;
 
-       // now we do reverse
-       // bring temp back at head 
-       temp = head;
-       count = 1;
-
-       if(left == 1){
+        if (left == 1) {
             head = stack.pop();
             temp = head;
 
-            while(!stack.isEmpty()){
-            temp.next = stack.pop();
-            temp = temp.next;
-            count++;
-       }
-       }else{
-            while(count != left-1){
+            while (!stack.isEmpty()) {
+                temp.next = stack.pop();
                 temp = temp.next;
-                count++; 
             }
-             // now temp is at left-1
-       if(count + 1 == left){
-            temp.next = stack.pop(); 
-            temp = temp.next;
-       }
-       count++;
-       // count at left
+        } else {
+            // Move temp to left - 1
+            while (count != left - 1) {
+                temp = temp.next;
+                count++;
+            }
 
-       while(!stack.isEmpty()){
             temp.next = stack.pop();
             temp = temp.next;
-            count++;
-       }
-       }
 
-      
-       //now count is right and stack is empty and temp is at  right
-       
+            while (!stack.isEmpty()) {
+                temp.next = stack.pop();
+                temp = temp.next;
+            }
+        }
+
+        // Connect reversed part to remaining list
         temp.next = temp2;
-        return head;  
-       
 
-       
-
-       
-
-
-
+        return head;
     }
 }
